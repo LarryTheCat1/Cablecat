4baa112c3ccf4c9f266245418bd4c041ad8f92b1
AnyKey <antoman1994@gmail.com>
2015-11-11 14:39:20 +0200

    reinit

---
+++ Cablecat/lng_russian.pl
@@ -0,0 +1,36 @@
+$_LIBRARY = 'Справочник';
+
+$_CABLE = 'Кабель';
+$_CABLE_TYPE = 'Тип кабеля';
+$_CABLE_TYPES = 'Типы кабеля';
+
+$_MODULES_COUNT = 'Количество модулей';
+$_FIBERS_COUNT = 'Количество волокон';
+
+$_COLOR_SCHEME = 'Цветовая схема';
+$_COLOR_SCHEMES = 'Цветовые схемы';
+
+$_IS_REQUIRED = 'является необходимым, но не указано';
+$_CANCEL = 'Отменить';
+
+$_CABLE_BIGHTS = 'Кабельные бухты';
+$_METERS_LEFT = 'Осталось, м';
+$_METERS_IN_BIGHT = 'Метров в бухте';
+$_ADDING_DATE = 'Дата добавления';
+
+$_LINK = 'Соединение';
+$_LINKS = 'Соединения';
+$_MANAGEMENT = 'Управление';
+$_LINK_MANAGEMENT='Управление соединениями';
+
+$_CONNECTER = 'Муфта';
+$_CONNECTERS = 'Муфты';
+
+$_BOX = 'Ящик';
+$_BOXES = 'Ящики';
+
+$_SPLITTER = 'Сплиттер';
+$_SPLITTERS = 'Сплиттеры';
+
+$_CROSS = 'Кросс';
+$_CROSSES = 'Кроссы';
